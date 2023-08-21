5c87985676e4c7706d2f60c155e40a5241e1d9f8
Kaushal Khachane <khachaneks22@gmail.com>
2023-08-21 20:37:11 +0530

    Added Delete Functionality to current user only

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,133 +1,143 @@
-import { useState, useEffect } from "react";
-import client, {
-  databases,
-  DATABASE_ID,
-  COLLECTION_ID_MESSAGES,
-} from "../appwriteConfig";
-import { ID, Query } from "appwrite";
-import { Trash2 } from "react-feather";
-import Header from "../components/Header";
+import { useState, useEffect } from 'react'
+import client, { databases, DATABASE_ID, COLLECTION_ID_MESSAGES } from '../appwriteConfig'
+import { ID, Query, Permission, Role } from 'appwrite';
+import Header from '../components/Header';
+import { useAuth } from '../utils/AuthContext';
+import { Trash2 } from 'react-feather'
+
 
 const Room = () => {
-  const [messages, setMessages] = useState([]);
-  const [messageBody, setMessageBody] = useState("");
+  const [messageBody, setMessageBody] = useState('')
+  const [messages, setMessages] = useState([])
+  const { user } = useAuth()
+
 
   useEffect(() => {
-    getMessages()
+    getMessages();
 
     const unsubscribe = client.subscribe(`databases.${DATABASE_ID}.collections.${COLLECTION_ID_MESSAGES}.documents`, response => {
-      // Callback will be executed on changes for documents A and all files.
-      console.log("Real Time: ", response);
 
       if (response.events.includes("databases.*.collections.*.documents.*.create")) {
-        console.log("MESSAGE WAS CREATED")
-        setMessages((prevState) => [response.payload, ...prevState]);
-
+        console.log('A MESSAGE WAS CREATED')
+        setMessages(prevState => [response.payload, ...prevState])
       }
 
       if (response.events.includes("databases.*.collections.*.documents.*.delete")) {
-        console.log("MESSAGE WAS DELETED!!!")
-
-        setMessages((prevState) =>
-          prevState.filter((message) => message.$id != response.payload.$id)
-        );
-
+        console.log('A MESSAGE WAS DELETED!!!')
+        setMessages(prevState => prevState.filter(message => message.$id !== response.payload.$id))
       }
     });
 
+    console.log('unsubscribe:', unsubscribe)
+
     return () => {
       unsubscribe();
-    }
-
+    };
   }, []);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    let payload = {
-      body: messageBody,
-    };
-
-    let response = await databases.createDocument(
-      DATABASE_ID,
-      COLLECTION_ID_MESSAGES,
-      ID.unique(),
-      payload
-    );
-
-    console.log("Created!: ", response);
-    // setMessages((prevState) => [response, ...prevState]);
-    setMessageBody("");
-  };
 
   const getMessages = async () => {
     const response = await databases.listDocuments(
       DATABASE_ID,
       COLLECTION_ID_MESSAGES,
-      [Query.orderDesc("$createdAt"), Query.limit(20)]
-    );
-    console.log("Response: ", response);
-    setMessages(response.documents);
-  };
+      [
+        Query.orderDesc('$createdAt'),
+        Query.limit(100),
+      ]
+    )
+    console.log(response.documents)
+    setMessages(response.documents)
+  }
 
-  const deleteMessage = async (message_id) => {
-    databases.deleteDocument(DATABASE_ID, COLLECTION_ID_MESSAGES, message_id);
-    // setMessages((prevState) =>
-    //   messages.filter((message) => message.$id != message_id)
-    // );
-  };
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    console.log('MESSAGE:', messageBody)
+
+    const permissions = [
+      Permission.write(Role.user(user.$id)),
+    ]
+
+    const payload = {
+      user_id: user.$id,
+      username: user.name,
+      body: messageBody
+    }
+
+    const response = await databases.createDocument(
+      DATABASE_ID,
+      COLLECTION_ID_MESSAGES,
+      ID.unique(),
+      payload,
+      permissions
+    )
+
+    console.log('RESPONSE:', response)
+
+    // setMessages(prevState => [response, ...prevState])
+
+    setMessageBody('')
+
+  }
+
+  const deleteMessage = async (id) => {
+    await databases.deleteDocument(DATABASE_ID, COLLECTION_ID_MESSAGES, id);
+    //setMessages(prevState => prevState.filter(message => message.$id !== message_id))
+  }
 
   return (
     <main className="container">
-    <Header />
+      <Header />
       <div className="room--container">
-        <form onSubmit={handleSubmit} id="message--form">
+
+        <form id="message--form" onSubmit={handleSubmit}>
           <div>
             <textarea
               required
-              maxLength="1000"
-              placeholder="Say Something..."
-              onChange={(e) => {
-                setMessageBody(e.target.value);
-              }}
+              maxlength="250"
+              placeholder="Say something..."
+              onChange={(e) => { setMessageBody(e.target.value) }}
               value={messageBody}
             ></textarea>
           </div>
 
           <div className="send-btn--wrapper">
-            <input
-              className="btn btn--secondary"
-              type="submit"
-              value="Send"
-              name=""
-              id=""
-            />
+            <input className="btn btn--secondary" type="submit" value="send" />
           </div>
         </form>
 
+
         <div>
-          {messages.map((message) => (
-            <div key={message.$id} className="message--wrapper">
+          {messages.map(message => (
+            <div key={message.$id} className={"message--wrapper"}>
               <div className="message--header">
-                <small className="message-timestamp">
-                  {new Date(message.$createdAt).toLocaleString()}
-                </small>
-                <Trash2
-                  className="delete--btn"
-                  onClick={() => {
-                    deleteMessage(message.$id);
-                  }}
-                />
+                <p>
+                  {message?.username ? (
+                    <span> {message?.username}</span>
+                  ) : (
+                    'Anonymous user'
+                  )}
+
+                  <small className="message-timestamp"> {new Date(message.$createdAt).toLocaleString()}</small>
+                </p>
+
+                {message.$permissions.includes(`delete(\"user:${user.$id}\")`) && (
+                  <Trash2 className="delete--btn" onClick={() => { deleteMessage(message.$id) }} />
+
+                )}
               </div>
-              <div className="message--body">
+
+              <div className={"message--body" + (message.user_id === user.$id ? ' message--body--owner' : '')}>
                 <span>{message.body}</span>
+
               </div>
+
+
             </div>
           ))}
         </div>
       </div>
     </main>
-  );
-};
+  )
+}
 
-export default Room;
+export default Room
