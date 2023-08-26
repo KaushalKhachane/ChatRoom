75762fd67e5d42b66c6b5f395b7b74d9c43b5c55
Kaushal Khachane <khachaneks22@gmail.com>
2023-08-26 11:35:01 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+  server: {
+    proxy: {
+      '/api': 'https://chat-theta-smoky.vercel.app/'
+    }
+  },
+
   plugins: [react()],
 })
+
